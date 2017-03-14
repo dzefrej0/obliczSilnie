2eff38c99c619996d72a25ef5e8dbc62e30482ac
Michał Piechota <mchpiechota@gmail.com>
2017-03-14 15:53:47 +0100

    Added new methods from previous project Almost working

--- src/main/java/com/eonetworks/SilniaController.java
+++ src/main/java/com/eonetworks/SilniaController.java
@@ -1,9 +1,11 @@
 package com.eonetworks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
@@ -14,17 +16,51 @@ import java.util.List;
 @Controller
 public class SilniaController {
 private SilniaService silniaService;
+@Autowired
+public SilniaController(SilniaService silniaService) {this.silniaService = silniaService;}
 
 
 
-    @RequestMapping(value = "silnia.s")
+    @RequestMapping(value = "silnia.s", method = RequestMethod.GET)
     public ModelAndView printAll() throws ServletException, IOException {
-//        List<BigInteger> all = silniaService.getAll();
         ModelAndView mav = new ModelAndView();
         mav.setViewName("silnia1");
-//        mav.addObject("silniaList", all);
+        List<BigInteger> all = silniaService.getAll();
+        mav.addObject("silniaList", all);
         return mav;
     }
+
+
+
+    @RequestMapping(value = "iterating.s", method = RequestMethod.POST)
+    public ModelAndView liczIteracja(@RequestParam int n) throws ServletException, IOException {
+//        silniaService.liczIteracja(n);
+        return new ModelAndView("redirect:silnia.s");
+    }
+
+    @RequestMapping(value = "recursion.s", method = RequestMethod.POST)
+    public ModelAndView obliczSilniaRekurencja(@RequestParam int n) throws ServletException, IOException {
+//        silniaService.obliczSilniaRekurencja(n);
+        if (n > 12000) {
+            throw new ArithmeticException("maxymalna wartość dla metody rkurencyjnej to 12000");
+        }
+        return new ModelAndView("redirect:silnia.s");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @RequestMapping(value  = "/index")
